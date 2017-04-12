2dc40c3d6593a83533161ee4ed8cef675e9b46ae
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-04-12 11:37:35 +0200

    Morpion

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var compteur = 0;
 var joueurEnCours = "";
-var tableau = [[0, 0, 0], 
-				[0, 0, 0],
-			 	[0, 0, 0]];
+var tableau = [[1, 2, 3], 
+				[4, 5, 6],
+			 	[7, 8, 9]];
 
 var joueur1 = 1;
 var joueur2 = 2;
@@ -19,19 +19,55 @@ $("td").click(function() {
 
 			var numCol = $(this).index();
 			var numLigne = $(this).parent().index();
-			//	console.log(numLigne +'/'+ numCol);
+				//console.log(numLigne +'/'+ numCol);
 
 			if (joueurEnCours === "p1") { 
 				$(this).css("background","white url(croix.png) no-repeat center");
-							
+				//$(this).prepend('<img src="croix.png" />')
+				tableau[numCol][numLigne] = "p1" ;
+				//console.log(tableau);
+			
 			}else{	
 				$(this).css("background","white url(rond.png) no-repeat center");
+				//$(this).prepend('<img src="rond.png" />')
+				tableau[numCol][numLigne] = "p2" ;
 			}       
 			
 		}
+		
+		if (tableau[0][0]===tableau[1][0] && tableau[1][0]===tableau[2][0] ){
+			alert("vous avez gagné");
+		}
 
-	//console.log(tableau);
+		if (tableau[0][1]===tableau[1][1] && tableau[1][1]===tableau[2][1] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[0][2]===tableau[1][2] && tableau[1][2]===tableau[2][2] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[0][0]===tableau[0][1] && tableau[0][1]===tableau[0][2] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[1][0]===tableau[1][1] && tableau[1][1]===tableau[1][2] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[2][0]===tableau[2][1] && tableau[2][1]===tableau[2][2] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[0][0]===tableau[1][1] && tableau[1][1]===tableau[2][2] ){
+			alert("vous avez gagné");
+		}
+
+		if (tableau[2][0]===tableau[1][1] && tableau[1][1]===tableau[0][2] ){
+			alert("vous avez gagné");
+		}
 });
+
 
 
 
